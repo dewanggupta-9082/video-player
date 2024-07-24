c6bbdf759737dabe64e64b6a482b8f7eb382ecc5
Dewang Gupta <avigupta8993@gmail.com>
2024-07-24 10:04:58 +0530

    dff

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import VideoList from "./components/shared/video-player/VideoList";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoList />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/shared/video-player/VideoPlayerCard.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import "./VideoPlayerCard.css";
+
+//const VideoPlayer = ({ videoUrl, description }) => {
+// const [isFullScreen, setIsFullScreen] = useState(false);
+
+// const toggleFullScreen = () => {
+//   setIsFullScreen(!isFullScreen);
+// };
+
+const VideoPlayerCard = ({ videoUrl, description, play }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (play) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }, [play]);
+
+  /* return (
+    <div
+      className={isFullScreen ? "video-card fullscreen" : "video-card"}
+      onClick={toggleFullScreen}
+    >
+      <ReactPlayer
+        url={videoUrl}
+        playing={isFullScreen}
+        controls={true}
+        width="100%"
+        height={isFullScreen ? "100%" : "200px"}
+      />
+      <div className="description-container">
+        <p className="description">{description}</p>
+      </div>
+    </div>
+  );
+}; */
+
+  return (
+    <div className="video-card">
+      <video ref={videoRef} src={videoUrl} width="100%" controls>
+        Your browser
+      </video>
+      <div className="description-container">
+        <p className="description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPlayerCard;
